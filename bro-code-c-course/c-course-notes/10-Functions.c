103056739c1da1a0e7bf2a48efcc713a7f081ceb
Rafael Henrique <rafaelhenrique.ra2@gmail.com>
2024-08-22 03:35:05 -0300

    studying-3AM

---
+++ bro-code-c-course/c-course-notes/10-Functions.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int birthday(void){
+    printf("\n Happy Birthday to you!");
+    printf("\n Happy Birthday to you!");
+    printf("\n Happy Birthday to you!\n");
+
+    return 0;
+}
+
+int main (void){
+    birthday();
+    birthday();
+    birthday();
+    
+    return 0;
+}
